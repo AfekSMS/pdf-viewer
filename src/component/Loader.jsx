57668750aa5b3d9eb9eed6ac1b80a21ea608c4a1
Afek <afek@sms2010.co.il>
2021-01-27 19:00:01 +0200

    pdf viewer with all the neede functions

---
+++ src/component/Loader.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Loader = ({ isLoading }) => {
+  if (!isLoading) return null;
+  return (
+    <div
+      id="loader"
+      className="d-flex justify-content-center align-items-center flex-column"
+    >
+      <img
+        src="https://www.sms2010.co.il/new_site/media/SMS_logo.png"
+        alt=""
+        className="mb-5 App-logo"
+      />
+      <p>Loading...</p>
+    </div>
+  );
+};
+export default Loader;
