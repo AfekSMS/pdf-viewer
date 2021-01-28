296e5937a8c6e4458da6d7f91b2aedc3d606301e
Afek <afek@sms2010.co.il>
2021-01-28 15:06:36 +0200

    added a side nave bar

--- src/component/PDFReader.jsx
+++ src/component/PDFReader.jsx
@@ -9,11 +9,14 @@ const PDFReader = () => {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
   const [isLoading, setisLoading] = useState(true);
+  //const [showFile, setshowFile] = useState(true);
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages);
     setisLoading(false);
   }
+
+  //const showFile = () => {};
 
   return (
     <div style={{ width: "100%" }}>
@@ -26,13 +29,47 @@ const PDFReader = () => {
         setPageNumber={setPageNumber}
         file="/assets/docs/file-sample.pdf"
       />
+
+      <nav>
+        <ul>
+          <li>
+            <a onClick={onDocumentLoadSuccess} href="#section-1">
+              file Name
+            </a>
+          </li>
+          <li>
+            <a href="#section-2">Section 2</a>
+          </li>
+          <li>
+            <a href="#section-3">Section 3</a>
+          </li>
+          <li>
+            <a href="#section-4">Section 4</a>
+          </li>
+          <li>
+            <a href="#section-5">Section 5</a>
+          </li>
+          <li>
+            <a href="#section-6">Section 6</a>
+          </li>
+          <li>
+            <a href="#section-7">Section 7</a>
+          </li>
+          <li>
+            <a href="#section-8">Section 8</a>
+          </li>
+          <li>
+            <a href="#section-9">Section 9</a>
+          </li>
+        </ul>
+      </nav>
       <section
         id="pdf-section"
         className="d-flex flex-column align-items-center"
       >
         <Document
           file="/assets/docs/file-sample.pdf"
-          onLoadSuccess={onDocumentLoadSuccess}
+          //onLoadSuccess={onDocumentLoadSuccess}
           className="document-layOut"
         >
           <Page pageNumber={pageNumber} scale={scale} />
